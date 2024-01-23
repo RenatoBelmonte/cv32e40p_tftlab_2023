53a667a1ddc2a0d9f2fbe2ff82779ce337599e1c
BELMONTE RENATO <s316792@po.polito.it>
2024-01-23 22:09:54 +0100

    cleaning

--- run.sh
+++ run.sh
@@ -3,16 +3,18 @@
 rm -rf logs
 mkdir logs
 
-#make clean
-#make synthesis/nangate45
-#make pt/generate_gsf
+make clean
+make synthesis/nangate45
+make pt/generate_gsf
+
+
 make compile_sbst > logs/compile_log 2>&1
 make questa/compile-timing >> logs/compile_log 2>&1
 make questa/lsim/gate-timing/shell >> logs/compile_log 2>&1
 make zoix/compile-timing >> logs/compile_log 2>&1
 for ((i=2; i<=20; i+=1)); do
-    # Use K as the result  f i multiplied by 0.5
-    K=$(bc <<< "scale=1; $i * 0.5")
+    # Use K as the result  f i multiplied by 0.25
+    K=$(bc <<< "scale=1; $i * 0.25")
     # Your code here using the variable K
     echo "Iteration $i: K = $K"
     make zoix/fgen/sdd K=$K > ./logs/log_$K 2>&1
